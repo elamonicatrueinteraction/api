41ee5ac2ea71e60d26b22eb7490fe7703585ca25
Agustin Cavilliotti <cavi21@gmail.com>
2018-03-02 18:53:23 -0300

    Add the update of the delivery date to the shippify webhook

--- app/services/gateway/shippify/webhooks/update_delivery.rb
+++ app/services/gateway/shippify/webhooks/update_delivery.rb
@@ -6,7 +6,7 @@ module Gateway
 
         def initialize(delivery, delivery_gateway_id)
           @delivery = delivery
-          @trip = @delivery.trip
+          @trip = delivery.trip
           @shippify_delivery_id = delivery_gateway_id
           @trip_data = get_trip_data
         end
@@ -26,6 +26,10 @@ module Gateway
 
               @trip.shipper = load_shipper if %w(assigned).include?(deliveries_status)
               @trip.shipper = nil if %w(processing broadcasting).include?(deliveries_status)
+
+              if deliveries_dates_changed?
+                @trip.steps = @new_steps_dates
+              end
 
               @trip.status = deliveries_status
               @trip.save!
@@ -80,6 +84,40 @@ module Gateway
 
         def load_shipper
           Shipper.find_by(gateway: 'Shippify', gateway_id: courier_data['id'])
+        end
+
+        # TO-DO: This logic looks very ugly and like a monkeypatch
+        # I'm leaving like this because is not going to be here for ever
+        def deliveries_dates_changed?
+          changed = false
+          steps = @trip.steps
+          @new_steps_dates = steps.clone
+
+          actions = %w(pickup dropoff)
+          deliveries_data.each do |delivery_data|
+            if reference_id = delivery_data['referenceId']
+              actions.each do |action|
+                time_window = delivery_data[action]['timeWindow']
+                step_schedule = step_schedule(steps, action, reference_id)
+                if time_window != step_schedule
+                  @new_steps_dates = @new_steps_dates.map do |new_step|
+                    if new_step['action'] == action && new_step['delivery_id'].to_s == reference_id.to_s
+                      new_step['schedule'] = time_window
+                    end
+                    new_step
+                  end
+                  changed = true
+                end
+              end
+            end
+          end
+
+          changed
+        end
+
+        def step_schedule(steps, action, reference_id)
+          step = steps.detect{|step| step['action'] == action && step['delivery_id'].to_s == reference_id.to_s } || {}
+          step['schedule']
         end
 
       end
