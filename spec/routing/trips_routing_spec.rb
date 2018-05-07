38f8a7a4fee463183994ee93d8734914df6c567f
Gustavo Valbuena <gust@Gustavos-MacBook-Air.local>
2018-05-07 11:10:32 -0300

    trips routing specs

--- spec/routing/trips_routing_spec.rb
+++ spec/routing/trips_routing_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe TripsController, type: :routing do
     it { expect(post: '/trips/1/broadcast').to route_to( routes_params.merge(controller: 'trips', action: 'broadcast') ) }
     it { expect(get: '/trips/1/broadcast').not_to be_routable }
   end
+
+  describe 'some resources under institutions' do
+    let(:routes_params){ { protocol: 'https', institution_id: '1' } }
+
+    it { expect(get: '/institutions/1/trips').to route_to( routes_params.merge(controller: 'trips', action: 'index') ) }
+    it { expect(get: '/institutions/1/trips/new').not_to be_routable }
+    it { expect(get: '/institutions/1/trips/1').not_to be_routable }
+    it { expect(get: '/institutions/1/trips/1/edit').not_to be_routable }
+    it { expect(post: '/institutions/1/trips').not_to be_routable }
+    it { expect(put: '/institutions/1/trips/1').not_to be_routable }
+    it { expect(delete: '/institutions/1/orders/1').not_to be_routable }
+  end 
 end
