cbc6dae836cd98fa37695210174f94efe0bbb2de
Agustin Cavilliotti <cavi21@gmail.com>
2018-09-25 16:54:45 -0300

    Define Access-Control-Expose-Headers in CORS to allow the pagination information

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :head, :delete]
+      methods: [:get, :post, :put, :patch, :head, :delete],
+      expose: ['Link', 'Per-Page', 'Total', 'X-Page']
   end
 end
