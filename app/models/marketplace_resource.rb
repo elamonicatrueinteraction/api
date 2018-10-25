66b46ead11355ae553dec56ad845045369865d17
Hugo Farji <hdf1986.6700@gmail.com>
2018-10-25 14:47:20 -0300

    Fixed linter

--- app/models/marketplace_resource.rb
+++ app/models/marketplace_resource.rb
@@ -1,6 +1,6 @@
 class MarketplaceResource < ActiveResource::Base
-  alias :read_attribute_for_serialization :send
+  alias read_attribute_for_serialization send
 
   self.site = MARKETPLACE_API_ENDPOINT
-  self.headers['Authorization'] = "Token token=#{MARKETPLACE_API_TOKEN}"
+  headers['Authorization'] = "Token token=#{MARKETPLACE_API_TOKEN}"
 end
