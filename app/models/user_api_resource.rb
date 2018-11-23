3954d663d14db45ed5ce04ffa723d0c3b72e5d76
Hugo Farji <hdf1986.6700@gmail.com>
2018-11-23 18:18:31 -0300

    TEsting to fix race condition

--- app/models/user_api_resource.rb
+++ app/models/user_api_resource.rb
@@ -14,12 +14,6 @@ class UserApiResource < ActiveResource::Base
   end
 
   def self.find_by(id: nil)
-    Rails.logger.info "Find User resource with #{headers['X-Network-ID']}"
-    Rails.logger.info(
-      "Find User resource with #{UserApiResource.current_network}"
-    )
-    ActiveResource::Base.headers['X-Network-Id'] =
-      UserApiResource.current_network
     find(id)
   rescue ActiveResource::ResourceNotFound, Errno::ECONNREFUSED
     nil
@@ -28,8 +22,6 @@ class UserApiResource < ActiveResource::Base
   def self.default_scope_by_network(current_network)
     return unless current_network
 
-    Rails.logger.info "User resource with current network #{current_network}"
-    Rails.logger.info "User resource with network #{headers['X-Network-ID']}"
     UserApiResource.current_network = current_network
     headers['X-Network-ID'] = current_network
   end
