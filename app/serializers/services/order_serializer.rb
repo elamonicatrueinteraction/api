1809c3874194aba0fb8581f2057ae7dde445b39c
Hugo Farji <hdf1986.6700@gmail.com>
2018-11-28 12:42:20 -0300

    Minor fix to order serializer

---
+++ app/serializers/services/order_serializer.rb
@@ -0,0 +1,18 @@
+class Services::OrderSerializer < ActiveModel::Serializer
+  attributes :id,
+    :marketplace_order_id,
+    :delivery_preference,
+    :expiration,
+    :amount,
+    :bonified_amount,
+    :created_at,
+    :updated_at,
+    :payments
+
+  def payments
+    ActiveModelSerializers::SerializableResource.new(
+      object.payments,
+      { each_serializer: PaymentSerializer }
+    ).as_json[:payments]
+  end
+end
