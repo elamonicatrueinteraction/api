dd6156839a0710f4959572b912df6511af0beffb
Hugo Farji <hdf1986.6700@gmail.com>
2018-11-29 15:31:46 -0300

    Fixed rubocop offfences

--- app/serializers/services/order_serializer.rb
+++ app/serializers/services/order_serializer.rb
@@ -1,18 +1,20 @@
-class Services::OrderSerializer < ActiveModel::Serializer
-  attributes :id,
-    :marketplace_order_id,
-    :delivery_preference,
-    :expiration,
-    :amount,
-    :bonified_amount,
-    :created_at,
-    :updated_at,
-    :payments
+module Service
+  class OrderSerializer < ActiveModel::Serializer
+    attributes :id,
+               :marketplace_order_id,
+               :delivery_preference,
+               :expiration,
+               :amount,
+               :bonified_amount,
+               :created_at,
+               :updated_at,
+               :payments
 
-  def payments
-    ActiveModelSerializers::SerializableResource.new(
-      object.payments,
-      { each_serializer: PaymentSerializer }
-    ).as_json[:payments]
+    def payments
+      ActiveModelSerializers::SerializableResource.new(
+        object.payments,
+        { each_serializer: PaymentSerializer }
+      ).as_json[:payments]
+    end
   end
 end
