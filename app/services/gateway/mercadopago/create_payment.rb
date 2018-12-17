c33c784cc4f7183cdc761978479ba8f50d5acada
Hugo D. Farji <hugo.farji@gmail.com>
2018-12-17 13:02:15 -0300

    Updated message for mercadopago

--- app/services/gateway/mercadopago/create_payment.rb
+++ app/services/gateway/mercadopago/create_payment.rb
@@ -51,7 +51,7 @@ module Gateway
 
       def payment_description
         if @payment.payable.is_a?(Order)
-          "NILUS/BAR - Pago por la orden ##{@payment.payable_id} e institución #{@payment.payable.receiver.name}"
+          "NILUS/BAR - Pago por la orden ##{@payment.payable_id}"
         else
           "NILUS - Pago por la entrega ##{@payment.payable_id}"
         end
