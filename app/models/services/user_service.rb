62bff8f15712c7fb3d07ca031eb92ea3458914a6
Hugo D. Farji <hugo.farji@gmail.com>
2018-12-18 12:57:05 -0300

    Fixed specs

--- app/models/services/user_service.rb
+++ app/models/services/user_service.rb
@@ -1,6 +1,10 @@
 module Services
   class UserService < Services::Base
     service_path "#{USER_SERVICE_ENDPOINT}/resources"
-    headers Authorization: "Token #{USER_SERVICE_TOKEN}"
+    headers Authorization: "Token token=#{USER_SERVICE_TOKEN}"
+
+    def read_attribute_for_serialization(attr)
+      send(attr)
+    end
   end
 end
