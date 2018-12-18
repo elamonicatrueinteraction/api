13ef7860ccd8420512e77415e7c4a1bfcd6fec9c
Hugo D. Farji <hugo.farji@gmail.com>
2018-12-18 12:57:34 -0300

    Fixed rubocop offences

--- spec/support/mocks/active_resource.rb
+++ spec/support/mocks/active_resource.rb
@@ -66,38 +66,38 @@ RSpec.configure do |config|
     }
 
     request_headers = {
-      Authorization:"Token token=#{USER_SERVICE_TOKEN}"
+      Authorization: "Token token=#{USER_SERVICE_TOKEN}"
     }
 
     ros_request_headers = {
-      Authorization:"Token token=#{USER_SERVICE_TOKEN}",
+      Authorization: "Token token=#{USER_SERVICE_TOKEN}",
       "X-Network-ID": "ROS"
     }
 
     # New mocks
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/institutions.json').with(headers: request_headers).to_return(body: [institution].to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/districts.json').with(headers: request_headers).to_return(body: districts.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions.json").with(headers: request_headers).to_return(body: {institutions: [institution]}.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions/#{institution[:id]}.json").with(headers: request_headers).to_return(body: {institution: institution}.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions/fake-id.json").with(headers: request_headers).to_return(body: nil, status: 404)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/users.json').with(headers: request_headers).to_return(body: users.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/users.json?email=dummy%40nilus.org').with(headers: request_headers).to_return(body: users.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/users/#{users.first[:id]}.json").with(headers: request_headers).to_return(body: users.first.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses/#{address[:id]}.json").with(headers: request_headers).to_return(body: address.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses/fake-id.json").with(headers: request_headers).to_return(body: nil, status: 404)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/addresses.json').with(headers: request_headers).to_return(body: {addresses: [address]}.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses.json?institution_id=#{institution[:id]}").with(headers: request_headers).to_return(body: {addresses: [address]}.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/institutions.json').with(headers: request_headers).to_return(body: [institution].to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/districts.json').with(headers: request_headers).to_return(body: districts.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions.json").with(headers: request_headers).to_return(body: { institutions: [institution] }.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions/#{institution[:id]}.json").with(headers: request_headers).to_return(body: { institution: institution }.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions/fake-id.json").with(headers: request_headers).to_return(body: nil, status: 404)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/users.json').with(headers: request_headers).to_return(body: users.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/users.json?email=dummy%40nilus.org').with(headers: request_headers).to_return(body: users.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/users/#{users.first[:id]}.json").with(headers: request_headers).to_return(body: users.first.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses/#{address[:id]}.json").with(headers: request_headers).to_return(body: address.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses/fake-id.json").with(headers: request_headers).to_return(body: nil, status: 404)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/addresses.json').with(headers: request_headers).to_return(body: { addresses: [address] }.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses.json?institution_id=#{institution[:id]}").with(headers: request_headers).to_return(body: { addresses: [address] }.to_json)
 
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/districts.json').with(headers: ros_request_headers).to_return(body: districts.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions.json").with(headers: ros_request_headers).to_return(body: [institution].to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions/#{institution[:id]}.json").with(headers: ros_request_headers).to_return(body: institution.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/institutions/fake-id.json").with(headers: ros_request_headers).to_return(body: nil, status: 404)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/users.json').with(headers: ros_request_headers).to_return(body: users.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/users.json?email=dummy%40nilus.org').with(headers: ros_request_headers).to_return(body: users.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/users/#{users.first[:id]}.json").with(headers: ros_request_headers).to_return(body: users.first.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses/#{address[:id]}.json").with(headers: ros_request_headers).to_return(body: address.to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses/fake-id.json").with(headers: ros_request_headers).to_return(body: nil, status: 404)
-    stub_request(:get, USER_SERVICE_ENDPOINT+'/resources/addresses.json').with(headers: ros_request_headers).to_return(body: [address].to_json)
-    stub_request(:get, USER_SERVICE_ENDPOINT+"/resources/addresses.json?institution_id=#{institution[:id]}").with(headers: ros_request_headers).to_return(body: [address].to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/districts.json').with(headers: ros_request_headers).to_return(body: districts.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions.json").with(headers: ros_request_headers).to_return(body: [institution].to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions/#{institution[:id]}.json").with(headers: ros_request_headers).to_return(body: institution.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/institutions/fake-id.json").with(headers: ros_request_headers).to_return(body: nil, status: 404)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/users.json').with(headers: ros_request_headers).to_return(body: users.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/users.json?email=dummy%40nilus.org').with(headers: ros_request_headers).to_return(body: users.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/users/#{users.first[:id]}.json").with(headers: ros_request_headers).to_return(body: users.first.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses/#{address[:id]}.json").with(headers: ros_request_headers).to_return(body: address.to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses/fake-id.json").with(headers: ros_request_headers).to_return(body: nil, status: 404)
+    stub_request(:get, USER_SERVICE_ENDPOINT + '/resources/addresses.json').with(headers: ros_request_headers).to_return(body: [address].to_json)
+    stub_request(:get, USER_SERVICE_ENDPOINT + "/resources/addresses.json?institution_id=#{institution[:id]}").with(headers: ros_request_headers).to_return(body: [address].to_json)
   end
 end
