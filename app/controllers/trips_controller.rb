1794b32af8967568215bb464e027025705a232ec
Hugo D. Farji <hugo.farji@gmail.com>
2019-01-08 21:42:55 -0300

    Performance optimization for report

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -85,7 +85,7 @@ class TripsController < ApplicationController
   def export
     optional_institution; return if performed?
 
-    stream_xlsx Exporters::Trips, trips: finder.result.limit(nil).includes(orders: :payments)
+    stream_xlsx Exporters::Trips, trips: finder.result.limit(nil).includes(orders: :payments, deliveries: :payments)
   end
 
   private
