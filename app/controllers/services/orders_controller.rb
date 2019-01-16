6445dfbc32f3dde7da3b806929ed7d2aa144fdbd
Hugo D. Farji <hugo.farji@gmail.com>
2019-01-16 13:50:30 -0300

    add reload for payments

--- app/controllers/services/orders_controller.rb
+++ app/controllers/services/orders_controller.rb
@@ -17,9 +17,10 @@ module Services
         order.payments.reload
         order.reload
 
+        Rails.logger.info 'Order'
+        Rails.logger.info Services::OrderSerializer.new(order).as_json
 
-
-        render json: Services::OrderSerializer.new(Order.find(order.id)).as_json, status: :created # 201
+        render json: Services::OrderSerializer.new(order).as_json, status: :created # 201
       else
         render json: { errors: service.errors }, status: :unprocessable_entity # 422
       end
