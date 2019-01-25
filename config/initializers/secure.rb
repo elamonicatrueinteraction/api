bb85a6accb5f6e5d25f92ff4373264f1c2aa920e
Hugo D. Farji <hugo.farji@gmail.com>
2019-01-25 12:49:17 -0300

    Fixed typo

--- config/initializers/secure.rb
+++ config/initializers/secure.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   # To prevent host header injection (http://carlos.bueno.org/2008/06/host-header-injection.html)
-  Rails.application.config.action_controller.default_url_options = { host: Raisl.application.secrets.hostname }
+  Rails.application.config.action_controller.default_url_options = { host: Rails.application.secrets.hostname }
   # config.action_controller.asset_host = ""
 end
