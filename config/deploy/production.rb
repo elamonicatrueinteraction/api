402aff3fbe86ec2d13f484d3a009fc28ab2bc8e0
Hugo D. Farji <hugo.farji@gmail.com>
2019-03-14 14:57:10 -0300

    Added deploy file

---
+++ config/deploy/production.rb
@@ -0,0 +1,9 @@
+set :stage, :production
+set :branch, :master
+
+server 'api.nilus.org', user: 'deploy', roles: %w{ app }
+
+set :rails_env, :production
+set :puma_env, :production
+set :puma_config_file, "#{shared_path}/config/puma.rb"
+set :puma_conf, "#{shared_path}/config/puma.rb"
