f5bccbba16be35c5c0ab590247b67a717fa19467
Tom Fernández <tomas.fernandez@greencodesoftware.com>
2019-08-01 14:53:25 -0300

    created env example, removed uncessary comments

--- config/initializers/00_config.rb
+++ config/initializers/00_config.rb
@@ -1,35 +1,4 @@
-# env = ENV['RAILS_ENV'] || Rails.env
-#
-# ALLOWED_APP_TOKENS = APP_CONFIG[env]['allowed_app_access_tokens'].freeze
-# ALLOWED_ORIGINS    = APP_CONFIG[env]['allowed_origins'].freeze
-#
-# aws = APP_CONFIG[env]['aws'].freeze
-# AWS_CREDENTIALS = aws['credentials'].freeze
-# AWS_REGION      = aws['region'].freeze
-# SNS_SETUP       = aws['sns'].freeze
-#
-# HOSTNAME = APP_CONFIG[env]['hostname'].freeze
-#
-# MERCADOPAGO        = APP_CONFIG[env]['mercadopago'].freeze
-# MERCADOPAGO_CONFIG = MERCADOPAGO['config'].freeze
-#
-# redis = APP_CONFIG[env]['redis'].freeze
 REDIS_DB   = Rails.application.secrets.redis_db
 REDIS_HOST = Rails.application.secrets.redis_host
 REDIS_PORT = Rails.application.secrets.redis_port
-redis_url  = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}".freeze
 $redis = Redis.new(host: REDIS_HOST, port: REDIS_PORT)
-#
-# services = APP_CONFIG[env]['services'].freeze
-# SERVICES_TOKENS = services.each_with_object({}) do |(name, data), _hash|
-#   _hash[name] = data['token']
-# end
-# MARKETPLACE_API_ENDPOINT = APP_CONFIG[env]['services']['marketplace']['endpoint']
-# MARKETPLACE_API_TOKEN = APP_CONFIG[env]['services']['marketplace']['token']
-#
-# ROLLBAR_ACCESS_TOKEN  = APP_CONFIG[env]['rollbar']['access_token'].freeze
-#
-# STAGING         = APP_CONFIG[env]['staging'].freeze || false
-#
-# SSL_ENABLED     = APP_CONFIG[env]['ssl_enabled'].freeze
-# SECURE_PROTOCOL = (SSL_ENABLED ? 'https' : 'http').freeze
