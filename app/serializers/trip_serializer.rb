bbfcf35bd66e11b1b5455e88dad9a2583b0e33da
Ezequiel Lamonica <lamonicaezequiel@gmail.com>
2020-01-29 17:22:42 -0300

    test serializaer wo orders

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -27,20 +27,18 @@ class TripSerializer < ActiveModel::Serializer
     :updated_at
 
   belongs_to :shipper, serializer: Simple::ShipperSerializer
-  has_many :orders, serializer: Deep::OrderSerializer
+  # has_many :orders, serializer: Deep::OrderSerializer
   has_many :audits, serializer: AuditSerializer
 
   # TO-DO: Please rethink this maybe in another place
   def status_detail
     case object.status
     when 'waiting_shipper'
-      # object.trip_assignments.where(closed_at: nil).last.try(:state)
-      object.status
+      object.trip_assignments.where(closed_at: nil).last.try(:state)
     when 'confirmed'
       'accepted'
     when 'on_going', 'completed'
-      # object.milestones.last.try(:name)
-      object.status
+      object.milestones.last.try(:name)
     else
       nil
     end
