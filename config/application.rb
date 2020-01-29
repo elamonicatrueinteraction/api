039a82f1c0effb5b1a9f088e1659820a4f5346b1
Ezequiel Lamonica <lamonicaezequiel@gmail.com>
2020-01-29 17:58:48 -0300

    install rack deflater

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,8 @@ module Api
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use Rack::Deflater
+
     config.middleware.use Rack::Attack if Rails.env.production?
 
   end
